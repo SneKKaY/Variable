5b5b656972fc4acbec242454f53a7975e2757028
SneKKaY <120265382+SneKKaY@users.noreply.github.com>
2024-10-26 13:10:06 +0300

    Delete Variable directory

--- Variable/variable.py
+++
@@ -1,7 +0,0 @@
-
-done_homework = 12
-spent_timeOfHour = 1.5
-nameOfCurse = 'Python'
-timeForOneTask = 0.125
-print("Курс:", nameOfCurse, " всего задач:", done_homework, " всего часов:", spent_timeOfHour,
-      " среднее время выполнения:", timeForOneTask  )
